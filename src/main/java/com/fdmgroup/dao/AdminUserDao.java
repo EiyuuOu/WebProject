e1fa52c9fbceb29ca3fa65cf6eac79a0f508765b
Nikola Bradas <nikola.bradas@fdmgroup.com>
2017-02-03 15:44:16 +0000

    Hoch die haende Wochenende

--- src/main/java/com/fdmgroup/dao/AdminUserDao.java
+++ src/main/java/com/fdmgroup/dao/AdminUserDao.java
@@ -8,6 +8,10 @@ public interface AdminUserDao extends UserDao {
 	
 	//http://stackoverflow.com/questions/11638155/java-session-filters-with-users-and-admin-login
 	//http://www.informit.com/articles/article.aspx?p=24253
+	//Check out SVN!!
+	//create admin class in model
+	// dont extend UserDao
+	// create 2 interfaces for admin (for user and for player)
 	public int deleteUser(String email);
 	public List<Users> getAllUsers();
 	public int changeBalance(String email, Users users);
